e3fa43ee95bb29af8c791111d6e9faeb546b7abb
EbBlovery <18217052735@163.com>
2017-09-17 19:20:49 +0800

    add curri database

--- router/lib/getCurri.js
+++ router/lib/getCurri.js
@@ -1,15 +1,48 @@
 var fetchUrpCurri = require('../../lib/fetchUrpCurri');
+var validateUser = require('./validateUser');
 
-async function getCurri(req,res){
+async function getCurri(req,res,type='cache'){
 	var {login} = req.cookies;
+	var { username,password } = req.app.locals.user;
+	if(type=="fresh"){
+        await getFresh(req,res,login)
+	}else{
+        var {pass} = await validateUser(req,username, password);
+        if(pass){
+            var data = await req.app.locals.db.Curri.findOne({username}).exec();
+            if(data){
+                var {curri} = data;
+                res.json({
+                	pass:true,
+                	type:'cache',
+                	curri
+                })
+            }else{
+            	await getFresh(req,res,login)
+            }
+        }else{
+        	res.json({
+        		data:{
+        			pass:false
+        		}
+        	})
+        }
+	}
+}
+
+
+async function getFresh(req,res,login){
+	var {username} = req.app.locals.user;
     var {data} = await fetchUrpCurri(req,res,login);
     var {pass,ret} = data;
-    console.log(data);
     if(pass){
-    	console
+        req.app.locals.db.Curri.update({username},{$set: {'curri':ret}},{upsert: true}).exec();
         res.json({
-        	pass:true,
-        	ret
+        	data:{
+        		pass:true,
+        		ret,
+        		type: fresh
+        	}
         })
     }else{
     	res.json({
@@ -18,6 +51,7 @@ async function getCurri(req,res){
     		}
     	})
     }
+
 }
 
 module.exports = getCurri;
