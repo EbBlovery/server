314ca53d4550b1dd094c059d0774150fd5be7044
EbBlovery <18217052735@163.com>
2017-09-17 13:43:10 +0800

    oldachi finish

--- router/lib/getOldAchi.js
+++ router/lib/getOldAchi.js
@@ -9,7 +9,6 @@ async function getOldAchi(req,res,type='cache'){
 	}else{
         var {pass} = await validateUser(req,username, password);
         if(pass){
-        	console.log(username);
         	var data = await req.app.locals.db.OldAchi.findOne({'username':username}).exec();
         	if(data){
         		var {ret} = data;
@@ -39,6 +38,7 @@ async function fetchFresh(req,res,login){
     var { pass, ret } = data;
     var { username } = req.app.locals.user;
     if(pass){
+         req.app.locals.db.OldAchi.update({ username },{$set: {'ret':ret}},{upsert: true}).exec()
     	 res.json({
     	 	data:{
     	 		pass: true,
@@ -46,7 +46,6 @@ async function fetchFresh(req,res,login){
         		type: 'cache'
     	 	}
     	 })
-         req.app.locals.db.OldAchi.update({ username },{$set: ret},{upsert: true}).exec()
     }else{
     	res.json({
     		data:{
--- router/newAchi.js
+++ router/newAchi.js
@@ -2,6 +2,7 @@ var getNewAchi = require('./lib/getNewAchi')
 
 async function getUrpNewAchi(req,res){
     var {data} = await getNewAchi(req,res);
+    console.log(data)
     res.json({
     	data
     })
