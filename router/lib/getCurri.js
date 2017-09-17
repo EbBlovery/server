04d81f4e3e6d7590373193f29a7582bd8d273255
EbBlovery <18217052735@163.com>
2017-09-17 16:18:12 +0800

    ...

--- router/lib/getCurri.js
+++ router/lib/getCurri.js
@@ -2,11 +2,13 @@ var fetchUrpCurri = require('../../lib/fetchUrpCurri');
 
 async function getCurri(req,res){
 	var {login} = req.cookies;
-    var {data} = fetchUrpCurri(req,res,login);
+    var {data} = await fetchUrpCurri(req,res,login);
     var {pass,ret} = data;
+    console.log(ret);
     if(pass){
         res.json({
         	data:{
+        		pass:true,
                 ret
         	}
         })
